1182ce0f1d66bbc424b57ab7b30735e96972b5e5
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-01-17 14:44:02 +0530

    new own solution

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -9,3 +9,10 @@ class Solution:
             else:
                 temp[num]=1
         return False
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            if nums[i] in nums[i+1:len(nums)]:
+                return True
+        return False
