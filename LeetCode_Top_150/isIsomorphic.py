c18d182e61b615139687d55e69c710fdbe20df68
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-06-16 12:30:16 +0530

    205. Isomorphic Strings

---
+++ LeetCode_Top_150/isIsomorphic.py
@@ -0,0 +1,12 @@
+# Time Complexity : O(n)
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        if map1 == map2:
+            return True
+        return False
