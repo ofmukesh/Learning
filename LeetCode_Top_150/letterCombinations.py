0b2c4f74bd81ca06b15d9e809ede2701aa828370
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-06-19 09:07:15 +0530

    17. Letter Combinations of a Phone Number

---
+++ LeetCode_Top_150/letterCombinations.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        KEY_MAP={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        res=[]
+        l=len(digits)
+
+        if l==0:
+            return res
+        elif l==1:
+            res=[c for c in KEY_MAP[digits[0]]]
+        else:
+            def backtrack(i,currStr):
+                if len(currStr)==len(digits):
+                    res.append(currStr)
+                    return 
+                
+                for c in KEY_MAP[digits[i]]:
+                    backtrack(i+1,currStr+c)
+                
+            if digits:
+                backtrack(0,'')
+        return res
+
+
+
