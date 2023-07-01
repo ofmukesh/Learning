bf2b3a4d8e6709e39a5633490f0fe33a9b4cf09f
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-07-01 10:13:40 +0530

    209. Minimum Size Subarray Sum

--- LeetCode_Top_150/minSubArrayLen.py
+++ LeetCode_Top_150/minSubArrayLen.py
@@ -6,11 +6,11 @@ class Solution:
         curr_sum=0
         
         while end<len(nums) and strt<len(nums):
-            curr_sum+=nums[end]
-            while curr_sum >= target and strt <= end:
+            curr_sum+=nums[end] # adding curr ele to sum
+            while curr_sum >= target and strt <= end: # shrinking current subarray from start if current sum is greater than target while current sum not less than target
                 min_l = min(min_l, end - strt + 1)
                 curr_sum-=nums[strt]
                 strt +=1
-            end+=1
+            end+=1 # expanding current subarray
 
         return min_l if min_l!=len(nums)+1 else 0
