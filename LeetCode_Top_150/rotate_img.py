6ca73de1d31c0757c5c5ffdb5b84080d59c1e1b4
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-07-07 15:19:30 +0530

    48. Rotate Image

---
+++ LeetCode_Top_150/rotate_img.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, x: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(x)):
+            for j in range(i,len(x)):
+                x[i][j],x[j][i]=x[j][i],x[i][j]
+        for i in range(len(x)):
+            x[i]=x[i][::-1]
