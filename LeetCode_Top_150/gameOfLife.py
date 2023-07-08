2103de2d53d4fb89f14d015d23dac985a7fa1bc0
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-07-08 10:03:26 +0530

    289. Game of Life

---
+++ LeetCode_Top_150/gameOfLife.py
@@ -0,0 +1,68 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                # count of neighbors of current cell
+                total_neighbors = 0
+
+                # current cell
+                curr=board[row][col]
+
+                # vertical and horizontal neighbors 
+                left=board[row][col-1] if col>0 else 0
+                right=board[row][col+1] if col<len(board[row])-1 else 0
+                top=board[row-1][col] if row>0 else 0
+                bottom=board[row+1][col] if row<len(board)-1 else 0
+
+                # add counts of vertical and horizontal neighbors to total_neighbors
+                # cell>1 refers to updated state
+                total_neighbors+=1 if 0<left<=2 else 0
+                total_neighbors+=1 if 0<right<=2 else 0
+                total_neighbors+=1 if 0<top<=2 else 0
+                total_neighbors+=1 if 0<bottom<=2 else 0
+
+                # diagonal neighbors 
+                top_left=board[row-1][col-1] if row>0 and col>0 else 0
+                top_right=board[row-1][col+1] if row>0 and col<len(board[row])-1 else 0
+                bottom_left=board[row+1][col-1] if row<len(board)-1 and col>0 else 0
+                bottom_right=board[row+1][col+1] if row<len(board)-1 and col<len(board[row])-1 else 0
+
+                # add counts of diagonal neighbors to total_neighbors
+                # cell>1 refers to updated state
+                total_neighbors+=1 if 0<top_left<=2 else 0
+                total_neighbors+=1 if 0<top_right<=2 else 0
+                total_neighbors+=1 if 0<bottom_left<=2 else 0
+                total_neighbors+=1 if 0<bottom_right<=2 else 0
+
+                # set to 2 for die and 3 for live
+                if curr==1 and (total_neighbors<2 or total_neighbors>3):
+                    board[row][col]=2
+                elif curr==0 and total_neighbors==3:
+                    board[row][col]=3   
+       
+        # updating the board to next generation state
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                curr=board[row][col]
+                
+                if curr==3:
+                    board[row][col]=1
+                elif curr==2:
+                    board[row][col]=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
