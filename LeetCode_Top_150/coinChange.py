d9ba9b6c83d036773c79938dcb1c99b6e9f6f3c8
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-08-13 16:30:29 +0530

    322. Coin Change

---
+++ LeetCode_Top_150/coinChange.py
@@ -0,0 +1,9 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp=[amount+1]*(amount+1)
+        dp[0]=0
+        for i in range(1,amount+1):
+            for j in coins:
+                if i-j>=0:
+                    dp[i]=min(dp[i],1+dp[i-j])
+        return dp[amount] if dp[amount]!=amount+1 else -1
