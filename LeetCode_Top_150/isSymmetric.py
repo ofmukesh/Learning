e66058824437f4efa5040ed74c0d35f9c333c110
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-08-15 13:54:19 +0530

    101. Symmetric Tree

---
+++ LeetCode_Top_150/isSymmetric.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def dfs(t1,t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val) and dfs(t1.left,t2.right) and dfs(t1.right, t2.left)
+        return dfs(root.left,root.right)
