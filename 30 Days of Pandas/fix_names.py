5f39561adc28e93af8b6997ba63ccce0eb4abd31
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-08-27 10:27:00 +0530

    1667. Fix Names in a Table

---
+++ 30 Days of Pandas/fix_names.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    # Apply the str.capitalize() function to fix the names
+    users['name'] = users['name'].str.capitalize()
+    
+    # Sort the result table by user_id in ascending order
+    result_df = users.sort_values(by='user_id', ascending=True)
+    
+    return result_df
