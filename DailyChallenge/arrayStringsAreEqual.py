b48ad887c3e6b7ebdaa54564916c00e957e48bcb
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-12-01 11:20:40 +0530

    1662. Check If Two String Arrays are Equivalent\

---
+++ DailyChallenge/arrayStringsAreEqual.py
@@ -0,0 +1,9 @@
+# Simple solution
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        word1="".join(word1)
+        word2="".join(word2)
+        return word1==word2
+
+# time complexity -> O(n*k)
+# space complexity -> O(n*k)
