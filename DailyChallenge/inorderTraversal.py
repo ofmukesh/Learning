966bb9a3e01b4c693ff6ed8399edd075926d3220
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-12-09 09:51:19 +0530

    94. Binary Tree Inorder Traversal

---
+++ DailyChallenge/inorderTraversal.py
@@ -0,0 +1,16 @@
+class Solution:
+def inorderTraversal(self, root: TreeNode) -> List[int]:
+
+    tree_vals = []
+    
+    def inorder(tree):
+        
+        if tree:
+            
+            inorder(tree.left)
+            tree_vals.append(tree.val)
+            inorder(tree.right)
+            
+    inorder(root)
+    
+    return tree_vals
