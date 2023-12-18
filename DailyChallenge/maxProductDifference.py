b83c013f3f412a3fffae5dfca2dd44b9e36b8f1e
Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-12-18 09:33:26 +0530

    1913. Maximum Product Difference Between Two Pairs

---
+++ DailyChallenge/maxProductDifference.py
@@ -0,0 +1,22 @@
+# the two largest and smallest elemetns product differnce is the answer 
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        biggest = 0
+        second_biggest = 0
+        smallest = inf
+        second_smallest = inf
+        
+        for num in nums:
+            if num > biggest:
+                second_biggest = biggest
+                biggest = num
+            else:
+                second_biggest = max(second_biggest, num)
+                
+            if num < smallest:
+                second_smallest = smallest
+                smallest = num
+            else:
+                second_smallest = min(second_smallest, num)
+        
+        return biggest * second_biggest - smallest * second_smallest
