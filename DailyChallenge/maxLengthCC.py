f83d2d38ddd4a636555c439f525471099eedf3d2
Mukesh <88231908+ofmukesh@users.noreply.github.com>
2024-01-23 13:40:31 +0530

    1239. Maximum Length of a Concatenated String with Unique Characters

---
+++ DailyChallenge/maxLengthCC.py
@@ -0,0 +1,24 @@
+# check for all possible combinations
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        result = [0]
+        self.dfs(arr, "", 0, result)
+        return result[0]
+
+    def dfs(self, arr, path, idx, result):
+        if self.isUniqueChars(path):
+            result[0] = max(result[0], len(path))
+
+        if idx == len(arr) or not self.isUniqueChars(path):
+            return
+
+        for i in range(idx, len(arr)):
+            self.dfs(arr, path + arr[i], i + 1, result)
+
+    def isUniqueChars(self, s):
+        char_set = set()
+        for c in s:
+            if c in char_set:
+                return False
+            char_set.add(c)
+        return True
