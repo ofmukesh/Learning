90a99af3570710a4d060f1580f1069769bcc185c
Mukesh <88231908+ofmukesh@users.noreply.github.com>
2024-01-24 13:19:58 +0530

    Data Type

---
+++ Strivers A2Z DSA Sheet/Basics/dataTypes.py
@@ -0,0 +1,12 @@
+from typing import *
+
+def dataTypes(type: str):
+    dummy = {
+        'Integer': 4,
+        'Long': 8 ,
+        'Float': 4 ,
+        'Double': 8 ,
+        'Character': 1
+    }
+
+    return dummy[type] 
