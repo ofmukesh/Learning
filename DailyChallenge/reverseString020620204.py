9c805216742b79fb6272400f9ab981e3ef2f368b
Mukesh <88231908+ofmukesh@users.noreply.github.com>
2024-06-02 15:08:52 +0530

    344. Reverse String

---
+++ DailyChallenge/reverseString020620204.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l=len(s)
+        for i in range(int(l/2)):
+            tmp=s[i]
+            s[i]=s[l-i-1]
+            s[l-i-1]=tmp
