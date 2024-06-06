d3f633c2638766c0fb60cc0b643462cb952fe9f2
Mukesh <88231908+ofmukesh@users.noreply.github.com>
2024-06-07 03:22:58 +0530

    846. Hand of Straights -- new solution added

--- DailyChallenge/isNStraightHand.py
+++ DailyChallenge/isNStraightHand.py
@@ -1,3 +1,4 @@
+#----------------------------------------------------------------------------------Solution 1 (O(n^2) and O(n)-----------------------------------------------------------------------------
 # Base Condition:
 #    When the cards are not possible to group: if we can't make groups of given groupSize by cards, this condition is possible when some elements are 
 #    left after grouping cards. 
@@ -30,6 +31,50 @@ class Solution:
             if len(result[curr])==groupSize:
                 i=0
                 curr+=1
+        
+        if len(hand)==0:
+            return True
+        else:
+            return False
+
+#----------------------------------------------------------------------------------Solution 1 (O(n^2) and O(1)-----------------------------------------------------------------------------
+# Base Condition:
+#    When the cards are not possible to group: if we can't make groups of given groupSize by cards, this condition is possible when some elements are 
+#    left after grouping cards. 
+ 
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # base conditions
+        if len(hand)%groupSize !=0 : return False
+
+        # make groups
+        last=None
+        resultL=0
+        hand.sort()
+
+        l=len(hand)
+        i=0
+        while i < l:
+            if resultL==0:
+                last=hand[i]
+                hand.pop(i)
+                l-=1
+                resultL+=1
+            else:
+                if last+1==hand[i]:
+                    last=hand[i]
+                    hand.pop(i)
+                    l-=1
+                    resultL+=1
+                elif last==hand[i]:
+                    i+=1
+                else:
+                    return False
+
+            if resultL==groupSize:
+                i=0
+                last=None
+                resultL=0
         
         if len(hand)==0:
             return True
