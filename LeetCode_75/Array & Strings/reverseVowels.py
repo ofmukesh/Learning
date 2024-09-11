0c69614487c125ae35ed6c59de5be35949ab5651
Mukesh <88231908+ofmukesh@users.noreply.github.com>
2024-09-11 10:22:04 +0530

    Updated: 345. Reverse Vowels of a String

--- LeetCode_75/Array & Strings/reverseVowels.py
+++ LeetCode_75/Array & Strings/reverseVowels.py
@@ -1,16 +1,14 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels=[]
-        s=list(s)
-        for idx,c in enumerate(s):
-            if c in 'aeiouAEIOU':
-                vowels.append(idx)
+        s=[w for w in s]
 
-        n=len(vowels)
-        m=n//2
-        for i in range(m):
-            tmp=s[vowels[i]] 
-            s[vowels[i]] = s[vowels[n-1-i]]
-            s[vowels[n-1-i]] = tmp
-
-        return ''.join(s)
+        for word in s:
+            if word in "aeiouAEIOU":
+                vowels.append(word)
+        
+        for i in range(len(s)):
+            if s[i] in "aeiouAEIOU":
+                s[i]=vowels.pop()
+        
+        return "".join(s)
